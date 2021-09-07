526310a90e6a80f1617f1600fee2ae26347585e4
rnagpal02 <rnagpal02@outlook.com>
2021-09-06 21:49:53 -0400

    Added command line args for velocity, acceleration, and wheelbase

--- MotionProfiling/setup/parse-args.cpp
+++ MotionProfiling/setup/parse-args.cpp
@@ -10,21 +10,39 @@ ParseArgs::ParseArgs(int argc, char *argv[]) {
     opterr = false;
 
     // Create command line options
-    struct option longOpts[] = { {"filename", required_argument, nullptr, 'f'},
-                                    {"help", no_argument, nullptr, 'h'} };
+    struct option longOpts[] = {{"filename", required_argument, nullptr, 'f'},
+                                {"velocity", required_argument, nullptr, 'v'},
+                                {"acceleration", required_argument, nullptr, 'a'},
+                                {"wheelbase", required_argument, nullptr, 'w'},
+                                {"help", no_argument, nullptr, 'h'}};
 
 
     // Vars to track options chosen
     bool stack_set = false, queue_set = false, output_set = false;
 
     bool gotFile = false;
+    bool gotVelocity = false;
+    bool gotAcceleration = false;
+    bool gotWheelbase = false;
 
     // Get all options
-    while ((option = getopt_long(argc, argv, "f:h", longOpts, &option_index)) != -1) {
+    while ((option = getopt_long(argc, argv, "f:v:a:w:h", longOpts, &option_index)) != -1) {
         switch(option) {
             case 'f':
                 filename = std::string(optarg);
                 gotFile = true;
+                break;
+            case 'v':
+                maxVelocity = strToDouble(optarg);
+                gotVelocity = true;
+                break;
+            case 'a':
+                maxAcceleration = strToDouble(optarg);
+                gotAcceleration = true;
+                break;
+            case 'w':
+                wheelbase = strToDouble(optarg);
+                gotWheelbase = true;
                 break;
             case 'h':
                 std::cerr << "This program reads in a CSV file containing waypoints for a \n" <<
@@ -32,6 +50,9 @@ ParseArgs::ParseArgs(int argc, char *argv[]) {
                                 "path to follow using quintic splines between the waypoints. The\n" <<
                                 "inputted CSV file should have an x, y, and theta for each waypoint.\n" <<
                                 "Usage: \'./MotionProfiling\n\t[--filename | -f]\n" <<
+                                            "\t[--velocity | -v]\n" <<
+                                            "\t[--acceleration | -a]\n" <<
+                                            "\t[--wheelbase | -w]\n" <<
                                             "\t[--help | -h]\'\n" <<
                                             "\tNote: [-f] should have a valid filename relative to the executable\n";
                 exit(1);
@@ -45,6 +66,18 @@ ParseArgs::ParseArgs(int argc, char *argv[]) {
 
     if(!gotFile) {
         std::cerr << "No filename provided. Please use --help for more info.\n";
+        exit(1);
+    }
+    if(!gotVelocity) {
+        std::cerr << "No velocity provided. Please use --help for more info.\n";
+        exit(1);
+    }
+    if(!gotAcceleration) {
+        std::cerr << "No acceleration provided. Please use --help for more info.\n";
+        exit(1);
+    }
+    if(!gotWheelbase) {
+        std::cerr << "No wheelbase provided. Please use --help for more info.\n";
         exit(1);
     }
 }
--- MotionProfiling/setup/parse-args.h
+++ MotionProfiling/setup/parse-args.h
@@ -8,6 +8,9 @@
 class ParseArgs {
 private:
     std::string filename; // filename containing waypoints in CSV format
+    double maxVelocity;
+    double maxAcceleration;
+    double wheelbase;
     std::vector<Waypoint> waypoints; // vector of waypoints from input file
 
     double strToDouble(const std::string &str);
@@ -16,5 +19,10 @@ public:
     ParseArgs(int argc, char *argv[]); // Parses command line args to get filename
 
     void parseFile(); // Parse CSV file to get waypoints
+
+    // Getters
+    double getVelocity() { return maxVelocity; }
+    double getAcceleration() { return maxAcceleration; }
+    double getWheelbase() { return wheelbase; }
     const std::vector<Waypoint>& getWaypoints() { return waypoints; }
 };
